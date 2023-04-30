a3cc058c6c87947404055cf063e5d143f224cc6e
ImranRH321 <imranrh247@gmail.com>
2023-04-30 13:19:47 +0600

    era-mem

---
+++ src/App.jsx
@@ -0,0 +1,14 @@
+import { useState } from "react";
+import "./App.css";
+import Header from "./componets/Header/Header";
+
+function App() {
+  return (
+    <>
+    <h1>ERa €John Repose </h1>
+    <Header></Header>
+    </>
+  );
+}
+
+export default App;
